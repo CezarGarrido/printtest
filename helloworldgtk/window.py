c899234c0b89903fe41c7ad5ec35c455fc31c271
Cezar Garrido Britez <cezar.britez@americanas.io>
2025-02-19 17:01:03 -0300

    chore: add fdb

--- helloworldgtk/window.py
+++ helloworldgtk/window.py
@@ -1,23 +1,12 @@
 from gi import require_versions
 require_versions({"Gtk": "4.0", "Adw": "1"})
 from gi.repository import Gtk
-from firebird.driver import connect
+import fdb
 
 class Window(Gtk.ApplicationWindow):
 
     def __init__(self, app):
         super().__init__(application=app)
         self.set_title("Hello World")
-
-        # Attach to 'employee' database/alias using embedded server connection
-        con = connect('employee', user='sysdba', password='masterkey')
-
-        # Attach to 'employee' database/alias using local server connection
-        from firebird.driver import driver_config
-        driver_config.server_defaults.host.value = 'localhost'
-        con = connect('employee', user='sysdba', password='masterkey')
-
-        # Set 'user' and 'password' via configuration
-        driver_config.server_defaults.user.value = 'SYSDBA'
-        driver_config.server_defaults.password.value = 'masterkey'
-        con = connect('employee')
+        con = fdb.connect(dsn="DSN", user="FBTEST_USER", password="FBTEST_PASSWORD")
+        # Attach to 'employee' database/alias using embedded server connecti
